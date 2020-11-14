8a167750cf7e6fa2167fb0a34f87834503737c6b
Federico Giorgi <contact@namelesstudio.com>
2020-11-14 14:56:52 +0100

    Added Weapon Sway

--- Assets/MixAndJam/Scripts/Weapons/WeaponSway.cs
+++ Assets/MixAndJam/Scripts/Weapons/WeaponSway.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class WeaponSway : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float intensity;
+    public float smooth;
+
+    private Quaternion originRotation;
+
+    private void Start()
     {
-        
+        originRotation = transform.localRotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        UpdateSway();
+    }
+
+    private void UpdateSway()
+    {
+        float mouseX = InputManager.hRightAxis;
+        float mouseY = InputManager.vRightAxis;
         
+        Quaternion xAdj = Quaternion.AngleAxis(intensity * mouseX, Vector3.up);
+        Quaternion yAdj = Quaternion.AngleAxis(intensity * mouseY, Vector3.right);
+        Quaternion targetRotation = originRotation * xAdj * yAdj;
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * smooth);
     }
 }
